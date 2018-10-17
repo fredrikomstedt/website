414c7f0bdcddc8bc47f1b9a7b92b14b443fa63a9
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2018-10-17 22:48:09 +0200

    Added a lot of things: x Background image with parallax scrolling x Buttons to traverse the interface with x Different font for title x Better looking github link x Some more styling

--- app/static/website.js
+++ app/static/website.js
@@ -9,8 +9,29 @@ var CURRENT_IMAGE = 0;
 
 //Main function
 $(document).ready(function() {
+    initiateTop();
     initiateAboutMe();
 });
+
+function initiateTop() {
+    $("#about-me-btn").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#about-me").offset().top},
+        'slow');
+    });
+
+    $("#experience-and-qualifications-btn").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#experience-and-qualifications").offset().top},
+        'slow');
+    });
+
+    $("#portfolio-btn").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#portfolio").offset().top},
+        'slow');
+    });
+}
 
 function getAge(dateString) {
     var today = new Date();
@@ -36,9 +57,8 @@ function startAboutMeImageFades() {
     setInterval(function() {
         $("#about-me-image").fadeOut(500, function() {
             CURRENT_IMAGE = (CURRENT_IMAGE + 1) % NUMBER_OF_IMAGES;
-            console.log('/static/pictures/about-me-' + CURRENT_IMAGE + '.jpg');
             $("#about-me-image").attr('src', '/static/pictures/about-me-' + CURRENT_IMAGE + '.jpg');
             $("#about-me-image").fadeIn(500);
         });
-    }, 5000);
+    }, 10000);
 }
