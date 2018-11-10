1adad6f036739eb038c21d6e370d2298c1c2bba7
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2018-11-10 14:54:13 +0100

    Started working on less email heavy email service

---
+++ app/send_email.py
@@ -0,0 +1,44 @@
+# This file contains functions used for sending messages over email.
+#
+# Author: Fredrik Omstedt
+# Date: 2018-11-10
+
+from config import email, password
+import traceback
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_message(file):
+    success = True
+    data = open(file, 'r').read()
+    fromaddr = email
+    toaddr = email
+
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = "Today's messages from your website!"
+        body = data
+
+        msg.attach(MIMEText(body, 'plain'))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(email, password)
+
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+    except:
+        traceback.print_exc()
+        success = False
+
+    if success:
+        open(file, 'w').close();
+
+    return success
+
+if __name__ == '__main__':
+    send_message('static/messages.txt')
