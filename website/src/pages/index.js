e8b50baa5d8b073d7035d83b51772ea2bdbfd457
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2020-07-02 15:06:38 +0200

    Fixed some text and added a shadow effect to make highlighted words pop

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -34,7 +34,7 @@ const Home = () => (
     <div className={homeStyles.wrapper}>
         Hi, I'm <ImageText text='Fredrik' image={Fredrik}/>! 
         <br/><br/>
-        I'm a <ImportantText text='software engineer'/> with an interest for <ImportantText text='game development'/> and <ImportantText text='machine learning'/>. I'm also a sports fan and an avid <ImageText text='golfer' image={Golfer}/>. Music is an important part of my life, and I love playing the <ImageText text='guitar' image={Guitar}/>.
+        I'm a <ImportantText text='software engineer'/> with an interest in <ImportantText text='game development'/> and <ImportantText text='machine learning'/>. I'm also a sports fan and an avid <ImageText text='golfer' image={Golfer}/>. Music is an important part of my life, and I love playing the <ImageText text='guitar' image={Guitar}/>.
         <br/><br/>
         Currently, I am working out of my home city of <ImportantText text='Stockholm, Sweden'/>, helping <ImportantText text='Topgolf'/> revolutionize the game of golf. If you want to know more about what I have done, check out my <HighlightText text='CV' positionalStyle={{top: 0, right: 125}}/> or my <HighlightText text='projects' positionalStyle={{top: 0, right: 0}}/>, send me an <HighlightText text='email' positionalStyle={{bottom: 15, left: 260}}/> or check out my other <HighlightText text='pages' positionalStyle={{bottom: 15, right: 260}}/>.
         <br/><br/>
