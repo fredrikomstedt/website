b6cb5965d71070c8a8ff42fda112bc09ebf44411
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2020-07-03 23:53:10 +0200

    Added a logo used on the home page and as a favicon

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -11,5 +11,23 @@ module.exports = {
                 component: require.resolve(`./src/components/layout.js`),
             },
         },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "Fredrik Omstedt",
+                short_name: "Fredrik Omstedt",
+                start_url: "/",
+                background_color: "#333333",
+                theme_color: "#ffffff",
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: "standalone",
+                icon: "src/images/website-logo.png", // This path is relative to the root of the site.
+                // An optional attribute which provides support for CORS check.
+                // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+                // Any invalid keyword or empty string defaults to `anonymous`
+                crossOrigin: `use-credentials`,
+            },
+          },
     ],
 }
