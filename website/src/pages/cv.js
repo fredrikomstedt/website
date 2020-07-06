5ede47912e5b56a7809e18058dc3adb838c639a6
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2020-07-06 19:09:30 +0200

    Added paper layout of CV to CV page
    No information has been added yet

--- website/src/pages/cv.js
+++ website/src/pages/cv.js
@@ -2,8 +2,10 @@ import React from "react"
 
 import SEO from "../components/seo"
 
+import cvStyles from './cv.module.css'
+
 const CVPage = () => (
-  <div>
+  <div className={cvStyles.wrapper}>
     <SEO title="CV" />
     <h1>CV</h1>
   </div>
