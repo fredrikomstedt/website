ae59e3b01931fbefe743e03139e3da5b7b051ede
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2021-11-10 20:39:37 +0100

    Updated information on website

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,57 +1,57 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-import ContextConsumer from "../../Context"
-import headerStyles from './header.module.css'
+import ContextConsumer from '../../Context';
+import headerStyles from './header.module.css';
 
-import Highlight from '../../images/highlight.png'
-import Logo from '../../images/website-logo.png'
+import Highlight from '../../images/highlight.png';
+import Logo from '../../images/website-logo.png';
 
-const HeaderTitle = ({siteTitle}) => (
-    <Link 
-        className={headerStyles.title}
-        to='/'
-    >
-        <img className={headerStyles.logo} src={Logo} alt=''/>
+const HeaderTitle = ({ siteTitle }) => (
+    <Link className={headerStyles.title} to="/">
+        <img className={headerStyles.logo} src={Logo} alt="" />
         <div className={headerStyles.siteTitleText}>{siteTitle}</div>
     </Link>
-)
+);
 
-const HeaderButton = ({link, text}) => (
-    <Link 
-        className={headerStyles.button}
-        to={link}
-    >
+const HeaderButton = ({ link, text }) => (
+    <Link className={headerStyles.button} to={link}>
         {text}
-        <HighlightCircle text={text}/>
+        <HighlightCircle text={text} />
     </Link>
-)
+);
 
-const HighlightCircle = ({text}) => (
+const HighlightCircle = ({ text }) => (
     <ContextConsumer>
-      {({ data }) => (
-        data.hover[text.toUpperCase()] ? <img className={headerStyles.highlightCircle} src={Highlight} alt=''/> : null
-      )}
+        {({ data }) =>
+            data.hover[text.toUpperCase()] ? (
+                <img
+                    className={headerStyles.highlightCircle}
+                    src={Highlight}
+                    alt=""
+                />
+            ) : null
+        }
     </ContextConsumer>
-  )
+);
 
 const Header = ({ siteTitle }) => (
     <header className={headerStyles.wrapper}>
-        <HeaderTitle siteTitle={siteTitle}/>
+        <HeaderTitle siteTitle={siteTitle} />
         <div className={headerStyles.buttonWrapper}>
-            <HeaderButton link='/cv' text='CV'/>
-            <HeaderButton link='/projects' text='Projects'/>
+            <HeaderButton link="/cv" text="CV" />
+            <HeaderButton link="/projects" text="Projects" />
         </div>
     </header>
-)
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+    siteTitle: ``,
+};
 
-export default Header
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,36 @@
-import React from "react"
+import React from 'react';
 
-import SEO from "../components/seo"
-import ContextConsumer from "../Context"
+import SEO from '../components/seo';
+import ContextConsumer from '../Context';
 
-import homeStyles from "./index.module.css"
+import homeStyles from './index.module.css';
 
-import Fredrik from '../images/fredrik.jpg'
-import Golfer from '../images/golfer.jpg'
-import Guitar from '../images/guitar.jpg'
-import Logo from '../images/website-logo-black.png'
+import Fredrik from '../images/fredrik.jpg';
+import Golfer from '../images/golfer.jpg';
+import Guitar from '../images/guitar.jpg';
+import Logo from '../images/website-logo-black.png';
 
-const ImageText = ({text, image}) => (
+const ImageText = ({ text, image }) => (
     <div className={homeStyles.imageText}>
         {text}
-        <img src={image} alt=''/>
+        <img src={image} alt="" />
     </div>
-)
+);
 
-const HighlightText = ({text}) => (
+const HighlightText = ({ text }) => (
     <ContextConsumer>
         {({ data, set }) => (
             <div
-                onMouseEnter={() => set({hover: {...data.hover, [text.toUpperCase()]: true}})}
-                onMouseLeave={() => set({hover: {...data.hover, [text.toUpperCase()]: false}})}
+                onMouseEnter={() =>
+                    set({
+                        hover: { ...data.hover, [text.toUpperCase()]: true },
+                    })
+                }
+                onMouseLeave={() =>
+                    set({
+                        hover: { ...data.hover, [text.toUpperCase()]: false },
+                    })
+                }
                 className={homeStyles.highlightText}
                 aria-hidden="true"
             >
@@ -30,30 +38,40 @@ const HighlightText = ({text}) => (
             </div>
         )}
     </ContextConsumer>
-)
+);
 
-const ImportantText = ({text}) => (
-    <div className={homeStyles.importantText}>
-        {text}
-    </div>
-)
+const ImportantText = ({ text }) => (
+    <div className={homeStyles.importantText}>{text}</div>
+);
 
 const Home = () => (
     <div className={homeStyles.wrapper}>
         <SEO title="Home" />
-        Hi, I'm <ImageText text='Fredrik' image={Fredrik}/>! 
-        <br/><br/>
-        I'm a <ImportantText text='software engineer'/> with an interest in <ImportantText text='game development'/> and <ImportantText text='machine learning'/>. I'm also a sports fan and an avid <ImageText text='golfer' image={Golfer}/>. Music is an important part of my life, and I love playing the <ImageText text='guitar' image={Guitar}/>.
-        <br/><br/>
-        Currently, I am working out of my home city of <ImportantText text='Stockholm, Sweden'/>, helping <ImportantText text='Topgolf'/> revolutionize the game of golf. If you want to know more about me and my work, check out my <HighlightText text='CV'/> or my <HighlightText text='projects'/>, send me an <HighlightText text='email'/>, or have a look at my other <HighlightText text='pages'/>.
-        <br/><br/>
+        Hi, I'm <ImageText text="Fredrik" image={Fredrik} />!
+        <br />
+        <br />
+        I'm a <ImportantText text="software engineer" /> with an interest in{' '}
+        <ImportantText text="full stack development" /> and{' '}
+        <ImportantText text="data analysis" />. I'm also a sports fan and an
+        avid <ImageText text="golfer" image={Golfer} />. Music is an important
+        part of my life, and I love playing the{' '}
+        <ImageText text="guitar" image={Guitar} />.
+        <br />
+        <br />
+        Currently, I am working out of my home city of{' '}
+        <ImportantText text="Stockholm, Sweden" />, helping{' '}
+        <ImportantText text="Topgolf" /> revolutionize the game of golf. If you
+        want to know more about me and my work, check out my{' '}
+        <HighlightText text="CV" /> or my <HighlightText text="projects" />,
+        send me an <HighlightText text="email" />, or have a look at my other{' '}
+        <HighlightText text="pages" />.
+        <br />
+        <br />
         Take care!
-        <img className={homeStyles.logo} src={Logo} alt=''/>
+        <img className={homeStyles.logo} src={Logo} alt="" />
     </div>
-)
+);
 
-const IndexPage = () => (
-    <Home/>
-)
+const IndexPage = () => <Home />;
 
-export default IndexPage
+export default IndexPage;
